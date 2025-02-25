460e0ed5f9ea39e46d67aa186576c8beaf748ce6
Fabiano <radbios@gmail.com>
2025-02-24 22:53:19 -0300

    feat: atividade 2 respondida

--- tasks/task-02-load-image-from-url/load_image_from_url.py
+++ tasks/task-02-load-image-from-url/load_image_from_url.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import cv2 as cv
+import requests
 
 def load_image_from_url(url, **kwargs):
     """
@@ -15,9 +16,32 @@ def load_image_from_url(url, **kwargs):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    try:
+        resp = requests.get(url, stream=True)
+        resp.raise_for_status()
+        image_array = np.asarray(bytearray(resp.content), dtype=np.uint8)
+        
+        flags = kwargs.get("flags", cv.IMREAD_COLOR)
+        image = cv.imdecode(image_array, flags)
+        
+        if image is None:
+            raise ValueError("Falha ao carregar a imagem.")
+
+        return image
+
+    except Exception as e:
+        print(f"Erro ao carregar imagem: {e}")
+        return None
     ### END CODE HERE ###
     
     return image
+
+# parser = argparse.ArgumentParser(description="Carrega uma imagem a partir de uma URL.")
+# parser.add_argument("--url", type=str, )
+# parser.add_argument("--kwargs", type=int, default=cv.IMREAD_COLOR)
+
+# args = parser.parse_args()
+
+# load_image_from_url(args.url, flags=args.kwargs)
 
 load_image_from_url()
